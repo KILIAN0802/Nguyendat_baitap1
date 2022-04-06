08c40e422f66bb42b9c9fec18b463fcd6846da3c
quanganh2001 <89558595+quanganh2001@users.noreply.github.com>
2022-04-06 10:31:26 +0700

    Fix source code due to my source code has similarity for other friends
    1. Add comments (Especially for getter and setter Class Student and Course)
    2. Input the mark (Using float not int)
    3. When input number 5, you can see the notification: Finish! Thank you!

--- student-mark-management-oop/2.student.mark.oop.py
+++ student-mark-management-oop/2.student.mark.oop.py
@@ -1,125 +1,162 @@
-students = []
-courses = []
-class Student:
-  def __init__(self, student_id, studentname, studentdob):
-    self.id = student_id
-    self.name = studentname
-    self.dob = studentdob
-    self.marks = {}
+# Set class student mark management system
+class System:
+  def __init__(self):
+    self.__students = []
+    self.__courses = []
   
-  def get_id(self):
-    return self.id
-  
-  def get_name(self):
-    return self.name
-  
-  def get_dob(self):
-    return self.dob
-  
-  def get_mark(self):
-    return self.marks
-  
-  def set_id(self, _id):
-    self.id = _id
-  
-  def set_name(self, name):
-    self.name = name
-  
-  def set_dob(self, dob):
-    self.dob = dob
-  
-  def set_mark(self, course, mark):
-    self.marks.update({course: mark})
-  
-  def displayStudent(self):
-    print("Student ID: " + self.id)
-    print("Student name: " + self.name)
-    print("Student DoB: " + self.dob)
-  
-  def displayMark(self, course):
-    print(self.name + "'s mark: " + str(self.marks.get(course)))
+  # Setter and getter
 
+  def setStudents(self):
+    self.__students = []
+    for i in range(self.__getInputNum__("students")):
+      print("Student " + str(i + 1) + ":")
+      self.__students.append(Student())
+  
+  def setCourses(self):
+    self.__courses = []
+    for i in range(self.__getInputNum__("courses")):
+      print("Course " + str(i + 1) + ":")
+      self.__courses.append(Course())
+  
+  def getStudents(self):
+    return self.__students
+  
+  def getCourses(self):
+    return self.__courses
+
+  @staticmethod
+  # Enter the number of students
+  def __getInputNum__(type_):
+    while True:
+      try:
+        i = int(input("Enter the number of " + type_ + ": "))
+        return i
+      except ValueError:
+        print("Invalid input! Please try again!")
+        pass
+  
+  # Input course name
+  def __getInputCourse__(self):
+    while True:
+      courseName = input("Enter course name: ")
+      for course in self.__courses:
+        if courseName == course.getName():
+          return course
+      print("Invalid course! Try again!")
+  
+  # Choose action
+  def __getActionId__(self):
+    print("==========")
+    print("Actions: ")
+    print(" (1) List Courses")
+    print(" (2) List Students")
+    print(" (3) Set Marks (given course)") # Choose only one course
+    print(" (4) Get Marks (given course)") # Choose only one course
+    print(" (5) Finish")
+    while True:
+      try:
+        i = self.__getInputNum__("Action")
+        if i in (1, 2, 3, 4, 5):
+          return i
+      except ValueError:
+        pass
+      print("Invalid input! Try again!")
+  
+  def __action__(self):
+    while True:
+      actionId = self.__getActionId__()
+      if actionId == 1:
+        for course in self.__courses:
+          print(course.getName())
+      elif actionId == 2:
+        for student in self.__students:
+          print(student.getName())
+      elif actionId == 3:
+        course = self.__getInputCourse__()
+        course.setMarks(self.__students)
+      elif actionId == 4:
+        course = self.__getInputCourse__()
+        course.getInfo()
+      else:
+        print("Finish! Thank you!")
+        break
+  
+  def run(self):
+    self.setStudents()
+    self.setCourses()
+    self.__action__()
+
+class Student:
+  # Enter your student information
+  def __init__(self):
+    self.__id = input("Enter id of Student: ")
+    self.__name = input("Enter name of Student: ")
+    self.__dob = input("Enter DoB of Student: ")
+  
+  # Getter and setter class Student
+  def setId(self, id_):
+    self.__id = id_
+  
+  def setName(self, name_):
+    self.__name = name_
+  
+  def setDoB(self, dob):
+    self.__dob = dob
+  
+  def getId(self):
+    return self.__id
+  
+  def getName(self):
+    return self.__name
+  
+  def getDoB(self):
+    return self.__dob
+  
+  def getInfo(self):
+    # Print info student
+    print("Student: " + self.__name + ":")
+    print(" * Id: " + self.__id)
+    print(" * Dob: " + self.__dob)
 
 class Course:
-  def __init__(self, course_id, coursename):
-    self.id = course_id
-    self.name = coursename
+  def __init__(self):
+    # Input your course
+    self.__name = input("Enter Course name: ")
+    self.__id = input("Enter Course ID: ")
+    self.__marks = []
   
-  def get_id(self):
-    return self.id
+  # Getter and setter class Course
+  def setName(self, name):
+    self.__name = name
   
-  def get_name(self):
-    return self.name
+  def setId(self, id_):
+    self.__id = id_
   
-  def set_id(self, id):
-    self.id = id
-  
-  def set_name(self, name):
-    self.name = name
-  
-  def displayCourse(self):
-    print("Course ID: " + self.id)
-    print("Course name: " + self.name)
-
-def numOfStudent():
-    std_num = int(input("How many student in class? "))
-    return std_num
-
-
-def studentInfo():
-  std_id = input("Student ID: ")
-  std_name = input("Student name: ")
-  std_dob = input("Student DoB: ")
-  return std_id, std_name, std_dob
-
-def numOfCourse():
-    course_num = int(input("Number of courses: "))
-    return course_num
-
-def courseInfo():
-  course_id = input("Course ID: ")
-  course_name = input("Course name: ")
-  return course_id, course_name
-
-
-def findCourseName(courses, course_id):
-  for course in courses:
-    if course.get_id() == course_id:
-      return course.get_name()
-  print("Error: Invalid ID!")
-
-if __name__ == "__main__":
-  student_num = numOfStudent()
-  print(student_num)
-  for i in range(0, student_num):
-    id, name, dob = studentInfo()
-    students.append(Student(id, name, dob))
-  
-  course_num = numOfCourse()
-  for i in range(0, course_num):
-    id, name = courseInfo()
-    courses.append(Course(id, name))
-  
-  print("Display students information:\n")
-  for student in students:
-    student.displayStudent()
-  
-  print("Display courses information:\n")
-  for course in courses:
-    course.displayCourse()
-  
-  a = 'b'
-  while a == 'b':
-    sel_course_id = input("Select a course ID: ")
-    sel_course = findCourseName(courses, sel_course_id)
-    print("Course name: " + sel_course + "\n")
+  def setMarks(self, students):
+    self.__marks = []
+    print("=========")
     for student in students:
-      mark = input("Enter " + student.name + "'s mark: ")
-      student.set_mark(sel_course, mark)
-    a = input("Select another course? y/n: ")
-    print("-------")
-  sel_course_id = input("Select a displayed course ID: ")
-  sel_course = findCourseName(courses, sel_course_id)
-  print(f"Display students' marks of course {sel_course}:\n")
-  for student in students:
-    student.displayMark(sel_course)
+      # Caution: Input the mark, you should type float, not int
+      mark = float(input("Enter mark of " + student.getName() + ": "))
+      self.__marks.append([student.getName(), mark])
+  
+  def getName(self):
+    return self.__name
+  
+  def getId(self):
+    return self.__id
+  
+  def getMarks(self):
+    return self.__marks
+  
+  def getInfo(self):
+    # Print name Course, ID and mark for this course
+    print("Course: " + self.__name + ":")
+    print(" * Id: " + self.__id)
+    print(" * Marks: ")
+    for i in range(len(self.__marks)):
+      print("     +   " + self.__marks[i][0] + ": " + str(self.__marks[i][1]))
+
+# Run the code
+System = System()
+System.run()
